7476e123c4f97509786caebb38ce1908ba94f4d7
martinhetschel <martin.hetschel@mach1kart.de>
2024-06-15 14:24:47 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo $1 $2
+
+cd ../$1
+docker compose down
+echo "down"
+docker run --rm \
+      -v "$2":/backup-volume \
+      -v "$(pwd)":/backup \
+      busybox \
+      tar -zcvf /backup/my-backup.tar.gz /backup-volume
+
+docker compose up -d
